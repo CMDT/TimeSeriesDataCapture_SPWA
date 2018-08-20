96db1cef87e8a5f50e1da9564ad8b3a14f31855a
Yusof Bandar <ybandar1902@gmail.com>
2018-08-20 15:31:08 +0100

    callback URI changed

--- src/app/components/authentication/oneDriveAuthenticationService.js
+++ src/app/components/authentication/oneDriveAuthenticationService.js
@@ -3,7 +3,7 @@ app.service('oneDriveAuthenticationService',['$log','odauthService','$window',fu
 
     var appInfo = {
         "clientId": '1a67f6f4-db2a-4298-8cf8-72946ac50669',
-        "redirectUri": "http://localhost:8080/callback.html",
+        "redirectUri": "https://timeseriesdatacapture-spwa.herokuapp.com/callback.html",
         "scopes": "user.read",
         "authServiceUri": "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
     }
