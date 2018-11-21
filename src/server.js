c2f833ef9fc8613be0e8554691f0ceaf1574a862
YusofBandar <ybandar1902@gmail.com>
2018-11-21 15:24:47 +0000

    Debugging : Heroku cannot find app folder

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 
 const app = express();
 
+writeClientConfig(getClientConfig());
 
 app.use(express.static(path.join(__dirname, '')));
 
@@ -38,4 +39,3 @@ var writeClientConfig = function(config){
   fs.writeFileSync('./app/client_config.js',client_config);
 }
 
-writeClientConfig(getClientConfig());
