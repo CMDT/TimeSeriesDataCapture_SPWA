69c142b0e2a8dbd1bdadb3853fd7f57dbbb013f5
YusofBandar <ybandar1902@gmail.com>
2018-11-22 18:53:37 +0000

    annotationInEditService commented

--- src/app/components/annotations/annotationInEditService.js
+++ src/app/components/annotations/annotationInEditService.js
@@ -1,9 +1,15 @@
 app.service('annotationInEditService', ['$rootScope', '$log', function () {
 
     var self = this;
-
+    //current annotationInEdit
     var annotationinEdit = {};
 
+    //adds new annotation which is currently being edited
+    // Storing only vital details : id, description and position (time)
+    // 
+    // Explaination : When user user clicks an annotation, these annotation details are stored within annotationInEdit
+    //                If user changes position of annotation or decription and cancels changes, these stored details are
+    //                used to revert changes made.
     self.addAnnotationInEdit = function (annotation) {
         if (annotationinEdit && annotationinEdit.id !== annotation.id) {
             annotationinEdit.id = annotation.id;
@@ -17,10 +23,12 @@ app.service('annotationInEditService', ['$rootScope', '$log', function () {
   
     }
 
+    //clears annotation 
     self.clearAnnotationInEdit = function(){
         annotationinEdit = {};
     }
 
+    //retrieves annotation
     self.getAnnotationInEdit = function(){
         return annotationinEdit;
     }
