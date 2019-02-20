083653f26e38c315b04de2efb685a8a700223b6f
YusofBandar <ybandar1902@gmail.com>
2019-02-20 16:08:38 +0000

    Correct Client Config Path

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ var writeClientConfig = function(config){
   var client_config = config;
   client_config = "var client_config = " + JSON.stringify(client_config);
   console.log(client_config);
-  fs.writeFileSync('./src/client_config.js',client_config);
+  fs.writeFileSync('./client_config.js',client_config);
 }
 
 writeClientConfig(getClientConfig());
